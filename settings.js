fd1dccfb63f45b1cd4fd507f74453e89879d51b7
cmdtoxic12 <promiseability845@gmail.com>
2025-03-14 05:32:24 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: '‼️<YOUR/LAST/WARNING>‼️', // Your name
-  ownerNumber: '233535679394', //Your number
+  botOwner: '‼️<SPARKLE>‼️', // Your name
+  ownerNumber: '233540790143', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
